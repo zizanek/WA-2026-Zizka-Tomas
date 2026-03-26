c32f2005fc3e0f8db9f449415349402eefaed2d6
Tomáš Žižka <tomas.zizka@tul.cz>
2026-03-26 11:33:08 +0100

    #8 Routování - Controler pro knihy a propojeni se uvodní strankou - SkA

---
+++ SkupinaA/BooksAppSkA/app/views/books/books_list.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script> -->
+    <title>Knihovna - Seznam knih</title>
+</head>
+<body>
+    <header>
+        <h1>Aplikace Knihovna</h1>
+        
+        <nav>
+            <ul>
+                <li><a href="/BooksAppSkA/public/index.php">Seznam knih (Domů)</a></li>
+                <li><a href="/BooksAppSkA/public/index.php?url=book/create">Přidat novou knihu</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <h2>Dostupné knihy</h2>
+        
+        <p>Zde se brzy objeví seznam knih načtený z databáze.</p>
+        
+        </main>
+
+    <footer>
+        <p>&copy; WA 2026 - Výukový projekt</p>
+    </footer>
+</body>
+</html>
